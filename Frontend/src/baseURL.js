49c9350fcd4566817c63800c69d661768b3340fc
krishna10k <krishnaprasadawala@gmail.com>
2024-04-24 15:29:48 +0530

    changes

--- Frontend/src/baseURL.js
+++ Frontend/src/baseURL.js
@@ -1,2 +1,3 @@
-export const baseURL = "https://restaurantordering-production.up.railway.app"
+// export const baseURL = "https://restaurantordering-production.up.railway.app"
+export const baseURL = "https://restaurant-s6l9.onrender.com"
 // export const baseURL = "http://localhost:3000"
